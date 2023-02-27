a9fc90566f7537b0b938aa56dfe884b15792a816
llugo15 <llugo@tamu.edu>
2023-02-27 08:22:03 -0600

    Connect to wifi and almost Firebase

--- main/main.c
+++ main/main.c
@@ -8,27 +8,105 @@
 #include "sensor.h"
 #include "connect.h"
 #include "nvs_flash.h"
+#include "string.h"
+#include "esp_http_client.h"
 #include "esp_log.h"
+#include "cJSON.h"
 
 #define DC_PIN 18
 #define SP_PIN 5
 
 static const char* TAG = "wifi_connect";
 
-void wifi_connect(void *parms)
-{
+extern const uint8_t certificate_pem[] asm("_binary_certificate_pem_start");
 
-    esp_err_t err = wifi_connect_sta("TAMU_IoT", "Blah", 50000);
-    if(err){
-        ESP_LOGE(TAG, "Failed to connect");
-        for(size_t i = 5; i > 0; i--)
-        {
-            printf("disconnecting in %d\n", i);
-            vTaskDelay(pdMS_TO_TICKS(1000));
-        }
-        wifi_disconnect();
+char *create_file()
+{
+    cJSON *jason_payload = cJSON_CreateObject();
+    cJSON *personalizations = cJSON_CreateArray();
+    cJSON_AddItemToObject(jason_payload, "personalizations", personalizations);
+
+    cJSON *personalization_0 = cJSON_CreateObject();
+    cJSON_AddItemToArray(personalizations, personalization_0);
+
+    cJSON *to = cJSON_CreateArray();
+    cJSON_AddItemToObject(personalization_0, "to", to);
+
+    cJSON *to_0 = cJSON_CreateObject();
+    cJSON_AddStringToObject(to_0, "email", "info@learnesp32.com");
+    cJSON_AddStringToObject(to_0, "name", "Mair");
+    cJSON_AddItemToArray(to, to_0);
+
+    cJSON_AddStringToObject(personalization_0, "subject", "Hello, World!");
+
+    cJSON *content = cJSON_CreateArray();
+    cJSON_AddItemToObject(jason_payload, "content", content);
+
+    cJSON *content_0 = cJSON_CreateObject();
+    cJSON_AddStringToObject(content_0, "type", "text/html");
+    cJSON_AddStringToObject(content_0, "value", "<h1>Heya!</h1>");
+    cJSON_AddItemToArray(content, content_0);
+
+    cJSON *from = cJSON_CreateObject();
+    cJSON_AddItemToObject(jason_payload, "from", from);
+    cJSON_AddStringToObject(from, "email", "info@learnesp32.com");
+    cJSON_AddStringToObject(from, "name", "Mair");
+
+    cJSON *reply_to = cJSON_CreateObject();
+    cJSON_AddItemToObject(jason_payload, "reply_to", reply_to);
+    cJSON_AddStringToObject(reply_to, "email", "info@learnesp32.com");
+    cJSON_AddStringToObject(reply_to, "name", "Mair");
+
+    char *payload_body = cJSON_Print(jason_payload);
+    printf("body: %s\n", payload_body);
+    cJSON_Delete(jason_payload);
+    return payload_body;
+}
+
+esp_err_t client_event_get_handler(esp_http_client_event_handle_t evt)
+{
+    switch(evt -> event_id)
+    {
+        case HTTP_EVENT_ON_DATA: 
+            printf("HTTP_EVENT_ON_DATA: %.*s\n", evt -> data_len, (char *)evt->data);
+            break;
+
+        default:
+        break;
     }
+    return ESP_OK;
     
+}
+
+void post_function()
+{
+    esp_http_client_config_t config_post = {
+        .url = "https://test-1ecd1-default-rtdb.firebaseio.com/preferences.json",
+        .method = HTTP_METHOD_POST,
+        .cert_pem = (const char *)certificate_pem,
+        .event_handler = client_event_get_handler};
+
+    esp_http_client_event_handle_t client = esp_http_client_init(&config_post);
+    esp_http_client_set_header(client, "Content-Type", "application/json");
+    //esp_http_client_set_header(client, "Authorization", "AIzaSyDWNPZ93LIPdr12eY5ZbfoDWgYSEdfVL08");
+
+    char *post_data = create_file(); // FIXME (what is being sent has to be jsons) 
+    esp_http_client_set_post_field(client, post_data, strlen(&config_post));
+
+    esp_err_t err = esp_http_client_perform(client);
+    if (err == ESP_OK)
+    {
+        ESP_LOGI(TAG, "HTTP GET status = %d\n", esp_http_client_get_status_code(client));
+    }
+    else
+    {
+        ESP_LOGE(TAG, "HTTP GET request failed: %s", esp_err_to_name(err));
+    }
+    if (post_data != NULL)
+    {
+        free(post_data);
+    }
+    esp_http_client_cleanup(client);
 }
 
 void app_main(void)
@@ -36,13 +114,19 @@ void app_main(void)
 
     ESP_ERROR_CHECK(nvs_flash_init());
     wifi_init();
-    xTaskCreate(wifi_connect, "wifi_connect", 1024 * 5, NULL, 5, NULL);
+    ESP_ERROR_CHECK(wifi_connect_sta("TAMU_IoT", "Blah", 100000));
+
+    vTaskDelay(2000/ portTICK_PERIOD_MS);
+    printf("WIFI was initiated............\n\n");
+
+    post_function();
+    wifi_disconnect();
     
     Sensor solar_panel_vol, dc_vol, battery_vol;
     double previous_power = 0.0;
 
     /*Configuring the gpio pin outputs which control the switches.
-    The default setting is high, to close the switches.*/
+    The default setting is high, to close the switches.
     gpio_pad_select_gpio(DC_PIN);
     gpio_pad_select_gpio(SP_PIN);
     gpio_set_direction(DC_PIN, GPIO_MODE_OUTPUT);
@@ -72,7 +156,7 @@ void app_main(void)
         
         //if battery is greater than 12.6V
         if(battery_vol_num >= 12.6){
-            /*Solar panel switch is closed, battery switch is opened.*/
+            //Solar panel switch is closed, battery switch is opened.
             gpio_set_level(DC_PIN, 0);
             printf("Status: Battery is charged, switch opened from battery.\n Load is supplied.");
         } 
@@ -96,8 +180,8 @@ void app_main(void)
                 }
             }
             else{
-                /*Solar panel switch is opened, 
-                the load will be supplied by the battery*/
+                //Solar panel switch is opened, 
+                //the load will be supplied by the battery
                 gpio_set_level(SP_PIN, 0);
             }
             if(battery_vol_num < 10.5){
@@ -105,7 +189,7 @@ void app_main(void)
                 printf("Status: Battery is low, there is not enough power to supply load.\n System has been disabled");
             }
         }
-    }
+    }*/
 }
 
 
